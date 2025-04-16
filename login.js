aa26d86d4b6be63b8ceaae02284921706eaee2b8
khusbu03 <kphulara@innow8apps.com>
2025-04-16 15:35:23 +0530

    login functionality

---
+++ login.js
@@ -0,0 +1,16 @@
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "Credentials missing" });
+
+        const existingUser = await User.findOne({ emailId: email });
+        if (!existingUser) return res.status(400).json({ message: "Invalid credentials" })
+
+        return res.status(200).json({ message: "Logged In " })
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Login failed" })
+
+    }
+}
